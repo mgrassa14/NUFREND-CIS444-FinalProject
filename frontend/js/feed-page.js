fc705684dc6987ae5b0d9ff8c1b41933294e9602
Maria Grassa <124839784+mgrassa14@users.noreply.github.com>
2026-04-23 18:32:00 -0700

    631 commit

--- frontend/js/feed-page.js
+++ frontend/js/feed-page.js
@@ -1,36 +1,11 @@
-// -----------------------------------------
-  // ❌ BACKEND NOT READY — SPA FETCH DISABLED
-  // -----------------------------------------
-  /*
-  const res = await fetch("/api/dogs");
-  const profiles = await res.json();
-  */
 
-  // -----------------------------------------
-  // ✅ TEMPORARY FAKE DATA (YOUR ORIGINAL DATA)
-  // -----------------------------------------
-// sample dog dog profiles
-const profiles = [
-  {
-    id: 1,
-    name: "Buddy",
-    image: "https://t3.ftcdn.net/jpg/02/74/06/48/240_F_274064877_Tuq84kGOn5nhyIJeUFTUSvXaSeedAOTT.jpg"
-  },
-  {
-    id: 2,
-    name: "Luna",
-    image: "https://karenhoglundphotography.com/wp-content/uploads/2020/09/Maltese-puppy.jpg"
-  },
-  {
-    id: 3,
-    name: "Max",
-    image: "https://vetmed.tamu.edu/news/wp-content/uploads/sites/9/2023/05/Puppy-Timeline-1-1024x768.jpeg"
-  }
-];
 // get the id for the feed box the profiles will go into
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   const feedBox = document.getElementById("feed-box");
   if (!feedBox) return;
+
+  const res = await fetch("http://localhost:3000/api/dogs");
+  const profiles = await res.json();
 
 // if (!feedBox) return;
 // for each profile in the array profiles...
