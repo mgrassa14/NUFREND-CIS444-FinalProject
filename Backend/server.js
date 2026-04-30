6fca760c7556b65c3aac9f580d22f236dfa72932
Maria Grassa <124839784+mgrassa14@users.noreply.github.com>
2026-04-29 20:42:46 -0700

    commented or deleted deployment attempt code

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,4 @@
 require('dotenv').config();
 const app = require('./app');
+app.listen(3000, () => console.log('running on :3000'));
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`running on :${PORT}`);
-});
